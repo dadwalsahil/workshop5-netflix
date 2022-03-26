b611bfb181863adda296846aa709643e035a6072
dadwalsahil <sahild@leewayhertz.com>
2022-03-26 16:14:57 +0530

    uploading netflix clone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import './App.css';
-import Row from './Row'
-import requests from './requests'
-import Banner from './Banner'
-import Navbar from './Navbar'
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+import YouTube from "react-youtube";
 function App() {
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
     <div className="app">
       <Navbar />
 
       <Banner />
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow={true}
       />
       <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <YouTube videoId="ZTI6T5M8Fj0" opts={opts} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} /> 
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import './Navbar.css'
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
 function Navbar() {
-    const [show, handleShow] = useState(false);
-    useEffect(() => {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+  }, []);
 
-        window.addEventListener("scroll", () => {
+  return (
+    <div className={`navbar ${show && "navbar_black"}`}>
+      <img
+        className="navbar_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
 
-            if (window.scrollY > 100) {
-                handleShow(true);
-            }
-            else {
-                handleShow(false);
-            }
-
-        });
-
-    }, []);
-
-    return (
-        <div className={`navbar ${show && "navbar_black"}`}>
-            <img
-                className='navbar_logo'
-                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-                alt="Netflix Logo" />
-
-            <img
-                className="navbar_avatar"
-                src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
-                alt="avatar" />
-
-        </div>
-    )
+      <img
+        className="navbar_avatar"
+        src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
+        alt="avatar"
+      />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
